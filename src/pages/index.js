1c50203e00785e31746094ac80114032218cc1b0
Lucas Cordeiro <cordeirolucas42@hotmail.com>
2021-07-27 18:08:01 -0300

    Update starter gatsby page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,13 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Imersão Autoconhecimento" />
-    <h1>Fala Voz</h1>
-    <p>Encarando o medo de se expressar</p>
+    <h1>Encarando o medo de se expressar</h1>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/Márcia Cipriano - Fala Voz (7).png"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="Cartaz Fala Voz"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>Facilitadora: <a href="https://www.instagram.com/marciaciprianoh/?hl=pt-br">Márcia Cipriano</a>
