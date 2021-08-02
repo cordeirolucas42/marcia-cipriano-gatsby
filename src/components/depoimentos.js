2d084915073559d750be805171cacb2b6d778077
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-02 12:07:09 -0300

    Formatada a seção de depoimentos.

--- src/components/depoimentos.js
+++ src/components/depoimentos.js
@@ -7,10 +7,18 @@ const Depoimentos = () => {
         <section className="bodySection">
         <h2>Depoimentos:</h2>
             <Row xs={1} md={2}>
-                <Col><iframe className="depoimentos" src="https://www.youtube.com/embed/f520SWi1aUQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Col>
-                <Col><iframe className="depoimentos" src="https://www.youtube.com/embed/hmRnyZ5J4V4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Col>
-                <Col><iframe className="depoimentos" src="https://www.youtube.com/embed/Wd0qHxknwTc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Col>
-                <Col><iframe className="depoimentos" src="https://www.youtube.com/embed/I9lxBoCQ34A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></Col>
+                <Col className="depoimentoCol">
+                    <iframe className="depoimentos" src="https://www.youtube.com/embed/f520SWi1aUQ?controls=0" title="Depoimento Isa Vida" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </Col>
+                <Col className="depoimentoCol">
+                    <iframe className="depoimentos" src="https://www.youtube.com/embed/hmRnyZ5J4V4?controls=0" title="Depoimento Rebeca" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </Col>
+                <Col className="depoimentoCol">
+                    <iframe className="depoimentos" src="https://www.youtube.com/embed/Wd0qHxknwTc?controls=0" title="Depoimento Juliana Sousa" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </Col>
+                <Col className="depoimentoCol">
+                    <iframe className="depoimentos" src="https://www.youtube.com/embed/I9lxBoCQ34A?controls=0" title="Depoimento Luana Dantas" frameborder="0"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </Col>
             </Row>
         </section>
     )
