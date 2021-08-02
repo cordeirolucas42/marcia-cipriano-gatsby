7ce250ad8b6ae4c47d79ca1740203df8e5fbadde
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-02 10:29:21 -0300

    Corrigido erro de digitação.

--- src/components/chamada.js
+++ src/components/chamada.js
@@ -8,7 +8,7 @@ const Chamada = () => {
             {/* PRIMEIRA OPÇÃO */}
             <StaticImage className="chamadaImg" src="../images/marcia-apontando__.png" alt="Márcia apontando"></StaticImage>
             <div className="chamadaBox">
-                <h6>Serão 12 horas de teoria e prática ao vivo no Zoom para que você desperte a coragem para encarar medo de se expressar publicamente e soltar a sua voz.</h6>
+                <h6>Serão 12 horas de teoria e prática ao vivo no Zoom para que você desperte a coragem para encarar o medo de se expressar publicamente e soltar a sua voz.</h6>
                 <a href="https://www.sympla.com.br/fala-voz--encarando-o-medo-de-se-expressar__1293670" className="btn chamadaBtn">Inscreva-se no Fala Voz</a>                
                 <a href="https://chat.whatsapp.com/FUHCL3l5VV7LbXLOHTNWot" className="btn chamadaBtn"><StaticImage className="iconeZap" src="../images/whatsapp.png" alt="Márcia apontando"></StaticImage><span>Junte-se ao grupo</span></a>
             </div>
