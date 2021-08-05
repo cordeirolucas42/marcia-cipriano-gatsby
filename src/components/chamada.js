f72b77b087713f828058635f349bc673d4711ff5
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-05 19:38:56 -0300

    Alterada chamada para vídeo.

--- src/components/chamada.js
+++ src/components/chamada.js
@@ -1,17 +1,10 @@
 import * as React from "react"
-import { StaticImage } from "gatsby-plugin-image"
 
 const Chamada = () => {
 
     return (
-        <section className="bodySection chamada">
-            {/* PRIMEIRA OPÇÃO */}
-            <StaticImage className="chamadaImg" src="../images/marcia-apontando__.png" alt="Márcia apontando"></StaticImage>
-            <div className="chamadaBox">
-                <h6>Serão 12 horas de teoria e prática ao vivo no Zoom para que você desperte a coragem para encarar o medo de se expressar publicamente e soltar a sua voz.</h6>
-                <a href="https://www.sympla.com.br/fala-voz--encarando-o-medo-de-se-expressar__1293670" className="btn chamadaBtn">Inscreva-se no Fala Voz</a>
-                <a href="https://chat.whatsapp.com/FUHCL3l5VV7LbXLOHTNWot" className="btn chamadaBtn"><StaticImage className="iconeZap" src="../images/whatsapp.png" alt="Márcia apontando"></StaticImage><span>Junte-se ao grupo</span></a>
-            </div>
+        <section className="bodySection apresentacaoBlock">
+            <iframe className="apresentacao" src="https://player.vimeo.com/video/583551933?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="apresentacao.mp4"></iframe>
         </section>
     )
 }
