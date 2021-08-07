b089644a60252c9c6ce729be5e623d2dbf1ee554
Lucas Cordeiro Herculano <cordeirolucas42@hotmail.com>
2021-08-07 09:48:12 -0300

    Modificado vídeo de chamada e a estilização da versão mobile.

--- src/components/chamada.js
+++ src/components/chamada.js
@@ -5,7 +5,7 @@ const Chamada = () => {
 
     return (
         <section className="bodySection apresentacaoBlock">
-            <iframe className="apresentacao" src="https://player.vimeo.com/video/583551933?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen title="apresentacao.mp4"></iframe>
+            <iframe className="apresentacao" src="https://www.youtube.com/embed/_3oODbs6UAg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <div className="buttonGroup">
                 <a href="https://www.sympla.com.br/fala-voz--encarando-o-medo-de-se-expressar__1293670" className="btn chamadaBtn">Inscreva-se no Fala Voz</a>
                 <a href="https://chat.whatsapp.com/FUHCL3l5VV7LbXLOHTNWot" className="btn chamadaBtn"><StaticImage className="iconeZap" src="../images/whatsapp.png" alt="Márcia apontando"></StaticImage><span>Junte-se ao grupo</span></a>
